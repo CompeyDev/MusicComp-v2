53ab0dc62a48d965e167bde013fbe0b5bf4ec3a0
DevComp <74418041+TheAwesomeCoder05@users.noreply.github.com>
2022-02-18 20:17:58 +0530

    create downloadlink() function

--- docs/js/cdn.js
+++ docs/js/cdn.js
@@ -3,6 +3,15 @@ randomlinks[0]="https://github.com/TheAwesomeCoder05/MusicComp-v2/tree/release";
 randomlinks[1]="https://cdn.jsdelivr.net/npm/musiccomp-discord-bot@0.2.9/";
 randomlinks[2]="https://unpkg.com/musiccomp-discord-bot@0.2.9/";
 
+var downloadlinks = [];
+randomlinks[0]="https://github.com/TheAwesomeCoder05/MusicComp-v2/archive/refs/heads/release.zip";
+randomlinks[1]="https://registry.npmjs.org/musiccomp-discord-bot/-/musiccomp-discord-bot-0.2.9.tgz";
+randomlinks[2]="https://github.com/TheAwesomeCoder05/MusicComp-v2/releases/download/v0.2.8/musiccomp-v0.2.8.zip/";
+
 function randomlink(){
   window.open(randomlinks[Math.floor(Math.random()*randomlinks.length)]);
+}
+
+function downloadlink(){
+  window.open(downloadlinks[Math.floor(Math.random()*downloadlinks.length)]);
 }
