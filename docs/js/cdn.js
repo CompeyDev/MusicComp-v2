dc41b3eb3e6c2097da67665b9ee9583e0854083b
DevComp <74418041+TheAwesomeCoder05@users.noreply.github.com>
2022-02-19 17:07:50 +0530

    fix randomlinks array numbering

--- docs/js/cdn.js
+++ docs/js/cdn.js
@@ -3,7 +3,7 @@ randomlinks[0]="https://github.com/TheAwesomeCoder05/MusicComp-v2/tree/release";
 randomlinks[1]="https://cdn.jsdelivr.net/npm/musiccomp-discord-bot@0.2.9/";
 randomlinks[2]="https://unpkg.com/musiccomp-discord-bot@0.2.9/";
 randomlinks[3]="https://cdn.devcomp.tk/musiccomp-discord-bot/-/musiccomp-v0.2.9.tgz";
-randomlinks[2]="https://cdn.devcomp.tk/musiccomp-discord-bot/-/musiccomp-v0.2.8.zip";
+randomlinks[4]="https://cdn.devcomp.tk/musiccomp-discord-bot/-/musiccomp-v0.2.8.zip";
 
 function randomlink(){
   window.open(randomlinks[Math.floor(Math.random()*randomlinks.length)]);
