ed40436a3edddb9c55ce0a9ab84268e88ba40918
DevComp <74418041+TheAwesomeCoder05@users.noreply.github.com>
2022-02-21 12:43:29 +0530

    update cdns list

--- docs/js/cdn.js
+++ docs/js/cdn.js
@@ -2,7 +2,6 @@ var randomlinks = [];
 randomlinks[0]="https://github.com/TheAwesomeCoder05/MusicComp-v2/tree/release";
 randomlinks[1]="https://cdn.jsdelivr.net/npm/musiccomp-discord-bot@0.2.10/";
 randomlinks[2]="https://unpkg.com/musiccomp-discord-bot@0.2.10/";
-randomlinks[3]="https://cdn.devcomp.tk/musiccomp-discord-bot/-/musiccomp-v0.2.10.tgz";
 randomlinks[4]="https://cdn.devcomp.tk/musiccomp-discord-bot/-/musiccomp-v0.2.10.zip";
 
 function randomlink(){
