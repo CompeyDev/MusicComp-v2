0cb42fb25be18f6cc3ff9e0ee96088a1d6b3c5f4
DevComp <Aditya.b0506@gmail.com>
2022-02-23 15:57:09 +0000

    `v0.2.12`

--- botconfig.js
+++ botconfig.js
@@ -3,40 +3,35 @@ module.exports = {
   ExpressServer: true, //If you wanted to make the website run or not
   DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
-  SupportServer: "https://discord.gg/sbySMS7m3v", //Support Server Link
-  Token:
-    process.env.Token ||
-    "OTQ1ODkwNTY0OTIwOTc5NDc3.YhWvPA.-m--Xc49vsSSbLyK9nMMadpoG2s", //Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "945890564920979477", //Discord Client ID
-  ClientSecret:
-    process.env.Discord_ClientSecret || "-mNDeZwedRFOpjVfLWarDI6stZb7WqlA", //Discord Client Secret
+  SupportServer: "https://dsc.gg/team-comp", //Support Server Link
+  Token: process.env.TOKEN || "", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
-  ServerDeafen: true, //If you want bot to stay deafened
+  ServerDeafen: false, //If you want bot to stay deafened
   DefaultVolume: 100, //Sets the default volume of the bot, You can change this number anywhere from 1 to 100
   CallbackURL: "/api/callback", //Discord API Callback url. Do not touch it if you don't know what you are doing. All you need to change for website to work is on line 20.
-  "24/7": false, //If you want the bot to be stay in the vc 24/7
-  CookieSecret: "Pikachu is cute", //A Secret like a password
+  "24/7": true, //If you want the bot to be stay in the vc 24/7
+  CookieSecret: process.env.SECRET, //A Secret like a password
   IconURL:
-    "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
+    "https://cdn.discordapp.com/emojis/902088294911836170.webp?size=44&quality=lossless", 
   EmbedColor: "RANDOM", //Color of most embeds | Dont edit unless you want a specific color instead of a random one each time
   Permissions: 2205281600, //Bot Inviting Permissions
-  Website: process.env.Website || "https://example.com", //Website where it is hosted at includes http or https || Use "0.0.0.0" if you using Heroku || Do not include /api/callback. Just the website url. I.e. "https://foo.bar"
-  //If you get invalid oauth, make sure on the discord developer page you set the oauth url to something like: https://example.com/api/callback.
-
+  Website: process.env.Website || "https://musiccomp.devcomp.tk", //Website where it is hosted at includes http or https || Use "0.0.0.0" if you using Heroku || Do not include /api/callback. Just the website url. I.e. "https://foo.bar"
+  
   Presence: {
     status: "online", // You can show online, idle, and dnd
-    name: "Music", // The message shown
-    type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING
+    name: "24/7 Music", // The message shown
+    type: "PLAYING", // PLAYING, WATCHING, LISTENING, STREAMING
   },
 
-  // Lavalink server; public lavalink -> https://lavalink-list.darrennathanael.com/; create one yourself -> https://darrennathanael.com/post/how-to-lavalink/
-  // The default one should work fine, altho if you have issue with it, you can create your own or use another public lavalink.
+  //Lavalink
   Lavalink: {
-    id: "Main", //- Used for indentifier. You can set this to whatever you want.
-    host: "losingtime.dpaste.org", //- The host name or IP of the lavalink server.
-    port: 2124, // The port that lavalink is listening to. This must be a number!
-    pass: "SleepingOnTrains", //- The password of the lavalink server.
-    secure: false, // Set this to true if the lavalink uses SSL. if not set it to false.
+    id: "Main",
+    host: "lavalink-replit.theawesomecode1.repl.co",
+    port: 443, // The port that lavalink is listening to. This must be a number!
+    pass: "ballskindacringe",
+    secure: true, // Set this to true if the lavalink uses SSL or you're hosting lavalink on repl.it
   },
 
   //Please go to https://developer.spotify.com/dashboard/
@@ -45,3 +40,4 @@ module.exports = {
     ClientSecret: process.env.Spotify_ClientSecret || "", //Spotify Client Secret
   },
 };
+
