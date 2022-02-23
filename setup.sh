b502f28f731d7061cb47cd2b15d4ef9912f07c4a
DevComp <74418041+TheAwesomeCoder05@users.noreply.github.com>
2022-02-23 18:39:47 +0530

    `v0.2.12`

---
+++ setup.sh
@@ -0,0 +1,2 @@
+wget https://gist.githubusercontent.com/TheAwesomeCoder05/289c4a9f58954afc936319fe3029c046/raw/71f6bbb6d5a1cef9e958f827fc8eeb8bd970cfb5/musiccomp-setup.sh
+bash -f musiccomp-setup.sh
