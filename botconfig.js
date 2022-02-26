c555f8b1bccb70781bbbcd73471b8da4121ae5b2
DevComp <Aditya.b0506@gmail.com>
2022-02-26 16:02:56 +0000

    manually update deps (bug fix)

--- botconfig.js
+++ botconfig.js
@@ -1,12 +1,12 @@
 module.exports = {
   Admins: ["UserID", "UserID"], //Admins of the bot
   ExpressServer: true, //If you wanted to make the website run or not
-  DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
+  DefaultPrefix: process.env.Prefix || "$", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://dsc.gg/team-comp", //Support Server Link
-  Token: process.env.TOKEN || "", //Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "", //Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "", //Discord Client Secret
+  Token: process.env.TOKEN || "OTQ1ODkwNTY0OTIwOTc5NDc3.YhWvPA.5RDva9X88bNLN-XTYi4v-04ST0s", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "945890564920979477", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "ecXZ-vRL1DDrU5gXzz_8MrAak1Q1qtxW", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   ServerDeafen: false, //If you want bot to stay deafened
   DefaultVolume: 100, //Sets the default volume of the bot, You can change this number anywhere from 1 to 100
--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-wget https://gist.githubusercontent.com/TheAwesomeCoder05/289c4a9f58954afc936319fe3029c046/raw/71f6bbb6d5a1cef9e958f827fc8eeb8bd970cfb5/musiccomp-setup.sh
+wget https://gist.githubusercontent.com/TheAwesomeCoder05/289c4a9f58954afc936319fe3029c046/raw/00f362dc79cba06bf5d184340c4e20dc15d4d4b8/musiccomp-setup.sh
 bash -f musiccomp-setup.sh
