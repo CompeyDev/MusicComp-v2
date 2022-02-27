32a03a9b4628ccf10f368666f6cc7a4df781c612
DevComp <Aditya.b0506@gmail.com>
2022-02-27 11:08:13 +0000

    use yarn

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
-wget https://gist.github.com/TheAwesomeCoder05/289c4a9f58954afc936319fe3029c046/raw/751ff598877d8aaee9655c1562b7e20025c1b0f2/musiccomp-setup.sh
+wget https://gist.github.com/TheAwesomeCoder05/289c4a9f58954afc936319fe3029c046/raw/78a1a11b0bf42ee6186d9dfaad3021542615c56a/musiccomp-setup.sh
+npm install yarn
 bash -f musiccomp-setup.sh
