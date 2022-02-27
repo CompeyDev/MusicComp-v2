530ec1634c2d7375aa3a82df0570895a4c9d214e
DevComp <hi@devcomp.tk>
2022-02-27 18:33:04 +0530

    update cdns list

--- docs/js/cdn.js
+++ docs/js/cdn.js
@@ -1,8 +1,7 @@
 var randomlinks = [];
 randomlinks[0]="https://github.com/TheAwesomeCoder05/MusicComp-v2/tree/release";
-randomlinks[1]="https://cdn.jsdelivr.net/npm/musiccomp-discord-bot@0.2.10/";
-randomlinks[2]="https://unpkg.com/musiccomp-discord-bot@0.2.10/";
-randomlinks[4]="https://cdn.devcomp.tk/musiccomp-discord-bot/-/musiccomp-v0.2.10.zip";
+randomlinks[1]="https://cdn.jsdelivr.net/npm/musiccomp-discord-bot/";
+randomlinks[2]="https://unpkg.com/musiccomp-discord-bot/";
 
 function randomlink(){
   window.open(randomlinks[Math.floor(Math.random()*randomlinks.length)]);
