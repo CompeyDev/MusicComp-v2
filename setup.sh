8bb042febd5f51ff6da0e464e14fa62413d068b7
DevComp <Aditya.b0506@gmail.com>
2022-02-27 11:09:18 +0000

    use yarn instead of npm

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-wget https://gist.github.com/TheAwesomeCoder05/289c4a9f58954afc936319fe3029c046/raw/78a1a11b0bf42ee6186d9dfaad3021542615c56a/musiccomp-setup.sh
+wget https://gist.github.com/TheAwesomeCoder05/289c4a9f58954afc936319fe3029c046/raw/21d603581a29aa1805a3851a98137b57ef32beef/musiccomp-setup.sh
 npm install yarn
 bash -f musiccomp-setup.sh
