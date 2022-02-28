b0d1a9a68159cc5a7a4bff457000311557bfbbdd
Compey <hi@devcomp.tk>
2022-02-28 22:36:56 +0530

    use new link for new user.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-wget https://gist.github.com/TheAwesomeCoder05/289c4a9f58954afc936319fe3029c046/raw/21d603581a29aa1805a3851a98137b57ef32beef/musiccomp-setup.sh
+wget https://gist.githubusercontent.com/CompeyDev/289c4a9f58954afc936319fe3029c046/raw/21d603581a29aa1805a3851a98137b57ef32beef/musiccomp-setup.sh
 npm install yarn
 bash -f musiccomp-setup.sh
