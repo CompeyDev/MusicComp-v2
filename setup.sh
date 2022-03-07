d816f452c6b584eb3e15bf9e8570a9781f742663
DevComp <hi@devcomp.tk>
2022-03-07 11:03:43 +0000

    add ascii to end of setup script :D

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-wget https://gist.githubusercontent.com/CompeyDev/289c4a9f58954afc936319fe3029c046/raw/21d603581a29aa1805a3851a98137b57ef32beef/musiccomp-setup.sh
+wget https://gist.githubusercontent.com/CompeyDev/289c4a9f58954afc936319fe3029c046/raw/b799a62abce72ad954b12647a8ac0e7ce189ae0c/musiccomp-setup.sh
 npm install yarn
 bash -f musiccomp-setup.sh
