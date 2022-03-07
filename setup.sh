d761f9d5941b8704a66c3aa7085dac2d44fbe1ce
DevComp <hi@devcomp.tk>
2022-03-07 12:02:42 +0000

    cleaner setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
+wget https://gist.github.com/CompeyDev/289c4a9f58954afc936319fe3029c046/raw/01894dc76ffbbdf7f0b2d361e2f1b24956b77982/musiccomp-setup.sh
 npm install yarn
 bash -f musiccomp-setup.sh
-
-
-
