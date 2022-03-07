bc1d7525c3b643517b4f35519804f3d7bd0e1136
Compey <hi@devcomp.tk>
2022-03-07 16:51:54 +0530

    fix grammar typo

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-wget https://gist.githubusercontent.com/CompeyDev/289c4a9f58954afc936319fe3029c046/raw/b799a62abce72ad954b12647a8ac0e7ce189ae0c/musiccomp-setup.sh
+wget https://gist.github.com/CompeyDev/289c4a9f58954afc936319fe3029c046/raw/28da51081b1017b3bcb893e856706d8400f832a1/musiccomp-setup.sh
 npm install yarn
 bash -f musiccomp-setup.sh
