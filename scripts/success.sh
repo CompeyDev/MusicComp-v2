ba1b44520a6fd7f013e220691dd1a2d84c013174
Compey <hi@devcomp.tk>
2022-03-07 19:36:07 +0530

    add more gap

--- scripts/success.sh
+++ scripts/success.sh
@@ -1,5 +1,8 @@
   cat <<'EOF'
            
+                               
+                               
+                               
                                 MusicComp is now ready, in case you 
                                 receieved an error, make sure you 
                                      filled out botconfig.js
