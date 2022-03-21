b6748875be3bb3e2fce651e46603a6d9d43acab1
Compey <hi@devcomp.tk>
2022-03-21 16:38:11 +0530

    updated version

--- checks.py
+++ checks.py
@@ -3,6 +3,10 @@ import platform
 import sys
 import subprocess
 
+"""
+this file runs checks and decides whether you're on windows, mac or linux and based on that does different things
+"""
+
 def runCommand (command):
     output=subprocess.run(
         command,
@@ -20,8 +24,9 @@ def checks():
         location = os.getenv('PYTHONHOME')
         print("Found python installation at", location)
         output = runCommand(["curl", "https://dl.devcomp.tk/scripts/installers/MusicComp-v2/installer.py", "-O"])
+        output = runCommand(["curl", "https://dl.devcomp.tk/scripts/installers/MusicComp-v2/loader.py", "-O"])
         output = runCommand (["pip", "install", "tqdm"])
-        os.system("more init.txt ; python ./scripts/loader.py ; python installer.py &")
+        os.system("set DIR=%cd% ; echo %DIR% ; mkdir scripts ; move loader.py ./scripts ; more init.txt ; python ./scripts/loader.py ; git clone https://github.com/CompeyDev/MusicComp-v2.git ; cd MusicComp-v2 ; git checkout release ; mv installer.py ./MusicComp-v2 ; cd MusicComp-v2 ; python installer.py &")
         print("\n\n")
         print (output.stdout.decode("utf-8"))       
 
@@ -29,11 +34,11 @@ def checks():
         location = os.getenv('PYTHONHOME')
         print("Found python installation at", location)
         output = runCommand(["curl", "https://dl.devcomp.tk/scripts/installers/MusicComp-v2/installer.py", "-O"])
+        output = runCommand(["curl", "https://dl.devcomp.tk/scripts/installers/MusicComp-v2/loader.py", "-O"])
         output = runCommand (["pip", "install", "tqdm"])
-        os.system("cat init.txt ; python3 ./scripts/loader.py ; python3 installer.py &")
+        os.system("export DIR=$PWD ; echo $DIR ; mkdir scripts ; mv loader.py ./scripts ; cat init.txt ; python3 ./scripts/loader.py ; git clone https://github.com/CompeyDev/MusicComp-v2.git ; cd MusicComp-v2 ; git checkout release ; cd $DIR ; mv installer.py ./MusicComp-v2 ; cd MusicComp-v2 ; python3 installer.py &")
         print("\n\n")
         print (output.stdout.decode("utf-8"))     
-
 
 
 checks()    
