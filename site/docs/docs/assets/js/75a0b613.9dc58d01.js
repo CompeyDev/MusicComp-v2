10273266c8520ee8566a3c0eb35fe29a0483b268
Compey <hi@devcomp.tk>
2022-03-22 20:13:12 +0530

    delete wrong build

--- site/docs/docs/assets/js/75a0b613.9dc58d01.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[210],{7309:function(e){e.exports=JSON.parse('{"permalink":"/docs/blog/tags/facebook","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
--- site/docs/docs/assets/js/b50dee57.fd6778ea.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[773],{9724:function(s){s.exports=JSON.parse('{"allTagsPath":"/docs/blog/tags","slug":"/docs/blog/tags/facebook","name":"facebook","count":1,"permalink":"/docs/blog/tags/facebook"}')}}]);
