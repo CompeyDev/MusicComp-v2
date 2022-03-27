1ba47a08d2aa243947e4a8cfbb89093e5c1064d1
DocsBot <hi@devcomp.tk>
2022-03-27 11:30:41 +0000

    🚀Update & Deploy Documentation

--- site/.docusaurus/routes.js
+++ site/.docusaurus/routes.js
@@ -4,31 +4,36 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
+    path: '/MusicComp-v2/docs/blog/archive',
+    component: ComponentCreator('/MusicComp-v2/docs/blog/archive','d01'),
+    exact: true
+  },
+  {
     path: '/MusicComp-v2/docs/markdown-page',
-    component: ComponentCreator('/MusicComp-v2/docs/markdown-page','785'),
+    component: ComponentCreator('/MusicComp-v2/docs/markdown-page','bd0'),
     exact: true
   },
   {
     path: '/MusicComp-v2/docs/docs',
-    component: ComponentCreator('/MusicComp-v2/docs/docs','506'),
+    component: ComponentCreator('/MusicComp-v2/docs/docs','0e6'),
     routes: [
       {
         path: '/MusicComp-v2/docs/docs/',
         component: ComponentCreator('/MusicComp-v2/docs/docs/','56a'),
         exact: true,
-        sidebar: "tutorialSidebar"
+        'sidebar': "tutorialSidebar"
       },
       {
         path: '/MusicComp-v2/docs/docs/tutorial-basics/host',
         component: ComponentCreator('/MusicComp-v2/docs/docs/tutorial-basics/host','72d'),
         exact: true,
-        sidebar: "tutorialSidebar"
+        'sidebar': "tutorialSidebar"
       }
     ]
   },
   {
     path: '/MusicComp-v2/docs/',
-    component: ComponentCreator('/MusicComp-v2/docs/','994'),
+    component: ComponentCreator('/MusicComp-v2/docs/','340'),
     exact: true
   },
   {
